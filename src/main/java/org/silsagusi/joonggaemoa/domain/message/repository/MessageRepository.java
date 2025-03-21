0d7341b4b337caf3356304f59fa002dee12ab647
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-03-21 14:09:13 +0900

    [#1] fix: 실행 오류 수정

--- src/main/java/org/silsagusi/joonggaemoa/domain/message/repository/MessageRepository.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/message/repository/MessageRepository.java
@@ -1,5 +1,6 @@
 package org.silsagusi.joonggaemoa.domain.message.repository;
 
+import org.silsagusi.joonggaemoa.domain.message.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
